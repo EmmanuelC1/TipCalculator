433b2abfa34b7f54855cf70efce4e4d9a1cfb26b
EmmanuelC1 <emcastillo@csumb.edu>
2020-01-26 16:59:25 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Emmanuel Castillo on 1/26/20.
+//  Copyright © 2020 Emmanuel Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
